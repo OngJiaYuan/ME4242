e9fddcf6fc23f7f338a9322ee8049cf25b51d25c
OngJiaYuan <ongjy98@outlook.com>
2022-10-04 20:20:23 +0800

    extra game_page

--- ME4242_QR/user_page/views.py
+++ ME4242_QR/user_page/views.py
@@ -5,3 +5,7 @@ from django.template import loader
 def index(request):
     template = loader.get_template('start_page.html')
     return HttpResponse(template.render())
+
+def index2(request):
+    template = loader.get_template('game_page.html')
+    return HttpResponse(template.render())
